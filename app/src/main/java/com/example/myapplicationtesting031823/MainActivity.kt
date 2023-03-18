e60a974c65a840579236390b4c318451c4689d31
John Ken Ompad <johnken2x13@gmail.con>
2023-03-18 10:47:16 +0800

    03/18/2023

---
+++ app/src/main/java/com/example/myapplicationtesting031823/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationtesting031823
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
