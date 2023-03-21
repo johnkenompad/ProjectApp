9d5805020ae7adeb9d4ff8d12d108ae320b19488
johnkenompad <113798974+johnkenompad@users.noreply.github.com>
2023-03-21 12:42:19 +0800

    Calculator adjustments

--- app/src/main/java/com/example/myprojectfinal/ListActivity.java
+++ app/src/main/java/com/example/myprojectfinal/ListActivity.java
@@ -1,16 +1,58 @@
 package com.example.myprojectfinal;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> itemList;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list1);
+
+        listView = findViewById(R.id.listView);
+        itemList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemList);
+        listView.setAdapter(adapter);
+
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+    }
+
+    private void addItem() {
+
+        EditText input = new EditText(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Item");
+        builder.setMessage("Enter new item:");
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String newItem = input.getText().toString();
+                itemList.add(newItem);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
     }
 }
